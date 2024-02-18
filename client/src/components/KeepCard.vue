c7366897c0cf571e8967818f198318541e4b056e
josh <josh_decime@yahoo.com>
2024-02-17 20:53:05 -0700

    keeps are displaying to the page with keep name & creator img

--- client/src/components/KeepCard.vue
+++ client/src/components/KeepCard.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="mb-4">
-        <img :src="keep.img" :alt="`Picture of ${keep.name}`" class="img-fluid keepImgStyling">
+    <div class="mb-4 relativePlacement">
+        <img :src="keep.img" :alt="`Picture of ${keep.name}`" class="img-fluid keepImgStyling gradient">
+        <p class="absolutePlacement">{{ keep.name }}</p>
+        <img :src="keep.creator.picture" :alt="`${keep.creator.name}'s profile picture`" class="profileImg">
     </div>
 </template>
 
@@ -22,5 +24,35 @@ export default {
 .keepImgStyling {
     border-radius: 15px;
     overflow: hidden;
+    // border: solid;
+    box-shadow: 3px 3px 4px;
+
+}
+
+.relativePlacement {
+    position: relative;
+
+}
+
+.absolutePlacement {
+    position: absolute;
+    bottom: 0;
+    margin: 10px;
+    font-weight: bolder;
+    color: var(--bs-text);
+    text-shadow: 2px 2px 3px black;
+}
+
+.profileImg {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin: 10px;
+    border-radius: 50%;
+    height: 3vh;
+}
+
+.gradient {
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.5) 100%)
 }
 </style>
